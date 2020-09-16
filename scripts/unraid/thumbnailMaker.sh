1356247c3fca7cb7be60ce7c8bf130b2fbd194cc
Jack <phyzicaly@hotmail.com>
2020-09-16 23:03:42 +0800

    fixes to thumbnail maker

--- scripts/unraid/thumbnailMaker.sh
+++ scripts/unraid/thumbnailMaker.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 folder="$1"
-docker run --rm -u $(id -u):$(id -g) -v "$folder":"$folder" \
--w "$folder" jrottenberg/ffmpeg -loglevel 0 -y -ss 00:02:00 -i "$f" \
--vframes 1 "${f%.mp4}-thumb".jpg
+for f in $folder/*.mp4;
+do
+  docker run --rm -u $(id -u):$(id -g) -v "$folder":"$folder" \
+  -w "$folder" jrottenberg/ffmpeg -loglevel 0 -y -ss 00:02:00 -i "$f" \
+  -vframes 1 "${f%.mp4}-thumb".jpg
+fi
+echo "Converting Thumbs"
+docker run --rm -v "$folder":/src --user=$(id -u):$(id -g) \
+madhead/imagemagick magick mogrify -resize 640x360 -format jpg "/src/*-thumb.jpg"
