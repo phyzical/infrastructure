e91f3e436be3df92004f32421f71b1b263d63a9f
Jack <phyzicaly@hotmail.com>
2020-09-16 23:05:12 +0800

    changed thumbnail maker ffmeg to 25 seconds

--- scripts/unraid/youtubeScraper.sh
+++ scripts/unraid/youtubeScraper.sh
@@ -53,7 +53,7 @@ else
           do
               echo "$f"
               docker run --rm -u $(id -u):$(id -g) -v "$folderPath":"$folderPath" -w "$folderPath" \
-              jrottenberg/ffmpeg -loglevel 0 -n -ss 00:02:00 -i "$f" -vframes 1 "${f%.mp4}-thumb".jpg
+              jrottenberg/ffmpeg -loglevel 0 -n -ss 00:00:25 -i "$f" -vframes 1 "${f%.mp4}-thumb".jpg
           done
           echo "Converting Thumbs"
           docker run --rm -v "$folderPath":/src --user=$(id -u):$(id -g) \
