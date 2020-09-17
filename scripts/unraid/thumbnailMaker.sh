0834b4956956877b97d3aa983e4fcfb819944877
Phyzical <phyzicaly@hotmail.com>
2020-09-17 18:15:12 +0800

    master: added fix for for

--- scripts/unraid/thumbnailMaker.sh
+++ scripts/unraid/thumbnailMaker.sh
@@ -8,7 +8,7 @@ do
     docker run --rm -u $(id -u):$(id -g) -v "$folder":"$folder" \
     -w "$folder" jrottenberg/ffmpeg -loglevel 0 -y -ss 00:02:00 -i "$f" \
     -vframes 1 "${f%.mp4}-thumb".jpg
-fi
+done
 echo "Converting Thumbs"
 docker run --rm -v "$folder":/src --user=$(id -u):$(id -g) \
 madhead/imagemagick magick mogrify -resize 640x360 -format jpg "/src/*-thumb.jpg"
