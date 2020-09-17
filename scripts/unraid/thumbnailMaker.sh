1ae7b8d30ede18ef92bcd0b4900185bb1578af6b
Phyzical <phyzicaly@hotmail.com>
2020-09-17 18:03:24 +0800

    master: added echos

--- scripts/unraid/thumbnailMaker.sh
+++ scripts/unraid/thumbnailMaker.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 folder="$1"
+echo "$folder"
 for f in $folder/*.mp4;
 do
-  docker run --rm -u $(id -u):$(id -g) -v "$folder":"$folder" \
-  -w "$folder" jrottenberg/ffmpeg -loglevel 0 -y -ss 00:02:00 -i "$f" \
-  -vframes 1 "${f%.mp4}-thumb".jpg
+    echo "$f"
+    docker run --rm -u $(id -u):$(id -g) -v "$folder":"$folder" \
+    -w "$folder" jrottenberg/ffmpeg -loglevel 0 -y -ss 00:02:00 -i "$f" \
+    -vframes 1 "${f%.mp4}-thumb".jpg
 fi
 echo "Converting Thumbs"
 docker run --rm -v "$folder":/src --user=$(id -u):$(id -g) \
