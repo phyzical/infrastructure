6cef4b8823c1356500cae4a95714aa154ad45820
Phyzical <phyzicaly@hotmail.com>
2021-04-11 11:45:49 +0800

    master: added sptifyscraper script

---
+++ scripts/unraid/spotifyScraper.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+
+source $DIR/commonFuncs.sh
+
+LOCKFILE="/tmp/spotifyInProgress.lock"
+
+trap 'failed_func $LOCKFILE "Spotify Failed!!" "Spotify Failed!! on line $LINENO"' ERR SIGTERM
+
+if [ -e $LOCKFILE ]
+then
+    echo "Spotify running already."
+    exit 1
+else
+    touch $LOCKFILE
+    message="Spotify Started"
+    notify normal $message "Spotify" $message
+    spotifyPath="/mnt/user/Downloads/spotify"
+    for sourceKey in "${!sources[@]}";
+    do
+        source=${sources[$sourceKey]}
+        
+        echo "Downloading ${sourceKey}"
+        docker run -it -u $(id -u):$(id -g) -v ${spotifyPath}:/download:rw --rm phyzical/spotify-dl "$source"
+    done
+    
+    echo "Finished Spotify Download!!"
+    rm -f $LOCKFILE
+    notify normal "Finished Spotify Download!!" "Finished Spotify Download, it took $(elapsed_time_message $SECONDS)" ""
+    exit 0
+fi
+
