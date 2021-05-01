2f295d2c831d587db0a67fc1fd489de070bd88d6
jack <jack.carpenter@agworld.com>
2021-05-01 21:24:11 +0800

    spotifyscraper fixes

--- scripts/unraid/spotifyScraper.sh
+++ scripts/unraid/spotifyScraper.sh
@@ -4,9 +4,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 source $DIR/commonFuncs.sh
 
 LOCKFILE="/tmp/spotifyInProgress.lock"
-
-#trap 'failed_func $LOCKFILE "Spotify Failed!!" "Spotify Failed!! on line $LINENO"' ERR SIGTERM
-
+trap 'failed_func $LOCKFILE "Spotify Failed!!" "Spotify Failed!! on line $LINENO"' ERR SIGTERM
+# declare -A sources=(
+#   ["artist"]="https://open.spotify.com/artist/0DGqabk3gsZWS8zEdgt1m5?si=ECdth_rMSnafUkGLskc08g"
+#   ["album"]="https://open.spotify.com/album/3XtEGVx9uh7J46nBzEc1VS?si=hUiwIxK0SZeCtWlEMUbmPA"
+#   ["playlist"]="https://open.spotify.com/playlist/46HnjD7FcxjwcTnbtoDVxI?si=je1jJpTvRCOl9jvo_tB8TA"
+#   ["song"]="https://open.spotify.com/track/2wBjgUXl43nSK7k3jVrSac?si=O6GOeMIkRyy9q-bBBYGZJQ"
+# )
 if [ -e $LOCKFILE ]
 then
     echo "Spotify running already."
@@ -16,8 +20,10 @@ else
     message="Spotify Started"
     notify normal $message "Spotify" $message
     spotifyPath="/mnt/user/Downloads/spotify"
-    for source in "$sources";
+    spotifyPath="$PWD/testfolder"
+    for sourceKey in "${!sources[@]}";
     do
+        source=${sources[$sourceKey]}
         echo "Downloading ${source}"
         docker run -u $(id -u):$(id -g) -v ${spotifyPath}:/download:rw --rm phyzical/spotify-dl "$source"
     done
