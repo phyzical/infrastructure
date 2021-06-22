558c0c7717addecb1b9f0fabfc1f425a7819d2dd
jack <jack.carpenter@agworld.com>
2021-06-22 19:46:25 +0800

    rollback

--- scripts/unraid/youtubeScraper.sh
+++ scripts/unraid/youtubeScraper.sh
@@ -30,7 +30,6 @@ else
         docker run --rm -u $(id -u):$(id -g) -v $youtubePath:/workdir:rw phyzical/yt-dlc \
         -f "$format" --download-archive "$channelName.txt" --write-thumbnail --add-metadata \
         --no-write-playlist-metafiles --compat-options no-youtube-unavailable-videos --sponsorblock \
-        --newline --console-title \
         --write-auto-sub --cookies=cookies.txt --write-info-json --convert-subs=srt --sub-lang "en" \
         --datebefore $oneMonthAgo --merge-output-format mp4 -o "$outputFormat" "$url"
         
