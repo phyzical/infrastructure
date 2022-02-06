d1309bb78bb76585741b15a8eaffd41b6c1be644
phyzical <phyzicaly@hotmail.com>
2022-02-06 20:28:22 +0800

    icnorrect match filter

--- scripts/unraid/youtubeScraper.sh
+++ scripts/unraid/youtubeScraper.sh
@@ -32,7 +32,7 @@ else
         -f "$format" --download-archive "$channelName.txt" --write-thumbnail --add-metadata \
         --no-write-playlist-metafiles --compat-options no-youtube-unavailable-videos --sponsorblock-remove "default" \
         --write-auto-sub --cookies cookies.txt --write-info-json --convert-subs=srt --sub-lang "en" \
-        --availability "public" --datebefore $oneMonthAgo --merge-output-format mp4 -o "$outputFormat" "$url"
+        --match-filter "availability = 'public'" --datebefore $oneMonthAgo --merge-output-format mp4 -o "$outputFormat" "$url"
         
         if [ -d "$processingPath" ]; 
         then
