859eec8751b7d44ba2ee5190d02abd477356d3b7
phyzical <phyzicaly@hotmail.com>
2022-05-14 14:18:19 +0800

    move to config

--- scripts/unraid/spotifyRipper.sh
+++ scripts/unraid/spotifyRipper.sh
@@ -19,7 +19,7 @@ else
     dockerImage="putty182/spotify-ripper"
     docker pull $dockerImage
 
-    docker run -i -v "$spotifyPath/config:/config" -v "$spotifyPath/songs:/music" putty182/spotify-ripper
+    docker run -v "$spotifyPath/config:/config" -v "$spotifyPath/songs:/music" --rm putty182/spotify-ripper
 
     chmod_unraid_file_permissions $spotifyPath
     
