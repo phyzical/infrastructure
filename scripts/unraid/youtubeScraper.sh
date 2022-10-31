4338af233aa29550b8a9a8b7feebe739ebb59b8e
phyzical <5182053+phyzical@users.noreply.github.com>
2022-11-01 00:04:14 +0800

    adjust filter

--- scripts/unraid/youtubeScraper.sh
+++ scripts/unraid/youtubeScraper.sh
@@ -33,7 +33,7 @@ else
         docker run --rm -u $(id -u):$(id -g) -v $youtubePath:/workdir:rw $dockerImage \
         -f "$format" --download-archive "$channelName.txt" --write-thumbnail --add-metadata \
         --no-write-playlist-metafiles --compat-options no-youtube-unavailable-videos --sponsorblock-remove "default" \
-        --write-auto-sub --cookies cookies.txt --write-info-json --convert-subs=srt --sub-lang "en" --ignore-no-formats \
+        --write-auto-sub --cookies cookies.txt --write-info-json --convert-subs=srt --sub-lang "en" --ignore-no-formats-error \
         --match-filter "!is_live & !was_live" \
         --datebefore $oneMonthAgo --merge-output-format mp4 -o "$outputFormat" "$url"
         
