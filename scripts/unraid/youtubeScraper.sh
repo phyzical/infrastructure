68acb6e294b6ea1c75b01fefa00f1a22a8dc4912
phyzical <5182053+phyzical@users.noreply.github.com>
2022-11-01 10:03:34 +0800

    adjust filter

--- scripts/unraid/youtubeScraper.sh
+++ scripts/unraid/youtubeScraper.sh
@@ -34,9 +34,9 @@ else
         -f "$format" --download-archive "$channelName.txt" --write-thumbnail --add-metadata \
         --no-write-playlist-metafiles --compat-options no-youtube-unavailable-videos --sponsorblock-remove "default" \
         --write-auto-sub --cookies cookies.txt --write-info-json --convert-subs=srt --sub-lang "en" --ignore-no-formats-error \
-        --match-filter "live_status!=not_live & live_status!=is_live & live_status!=is_upcoming & live_status!=was_live & live_status!=post_live" \
+        --match-filter "!is_live & !was_live" \
         --datebefore $oneMonthAgo --merge-output-format mp4 -o "$outputFormat" "$url"
-        
+
         if [ -d "$processingPath" ]; 
         then
             if ls $processingPath/*.webp 1> /dev/null 2>&1;
