8b92ffa66bd7177d91360eb519fecd3e112ea34d
phyzical <5182053+phyzical@users.noreply.github.com>
2023-10-22 15:44:10 +0800

    supress the error message another way

--- scripts/unraid/commonFuncs.sh
+++ scripts/unraid/commonFuncs.sh
@@ -50,15 +50,13 @@ move_episodes_to_season_folders () {
   nonSeasonFolders=$3
   for year in ${years[@]};
   do
-    if [ -d "$sourcePath/$year" ]; then
-      if find $sourcePath/$year*; then
-        if [[ " ${nonSeasonFolders[*]} " =~ " ${sourcePath} " ]]; then
-          echo "moving '$sourcePath/$year*' to '$destinationPath/Season 1/'"
-          mkdir -p $destinationPath/Season\ 1 && mv $sourcePath/$year* $destinationPath/Season\ 1/
-        else
-          echo "moving '$sourcePath/$year*' to '$destinationPath/Season $year/'"
-          mkdir -p $destinationPath/Season\ $year && mv $sourcePath/$year* $destinationPath/Season\ $year/
-        fi
+    if find $sourcePath/$year* 2>/dev/null; then
+      if [[ " ${nonSeasonFolders[*]} " =~ " ${sourcePath} " ]]; then
+        echo "moving '$sourcePath/$year*' to '$destinationPath/Season 1/'"
+        mkdir -p $destinationPath/Season\ 1 && mv $sourcePath/$year* $destinationPath/Season\ 1/
+      else
+        echo "moving '$sourcePath/$year*' to '$destinationPath/Season $year/'"
+        mkdir -p $destinationPath/Season\ $year && mv $sourcePath/$year* $destinationPath/Season\ $year/
       fi
     fi
   done
