9fbd00e48f6fb24cceec2ded4f8e5dc133cdc581
phyzical <5182053+phyzical@users.noreply.github.com>
2024-09-21 18:54:51 +0800

    add medal script

---
+++ scripts/unraid/medalScraper.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+source "$DIR"/commonFuncs.sh
+
+LOCKFILE="/tmp/medalInProgress.lock"
+
+trap 'failed_func $LOCKFILE "Medal Failed!! with code ($?)" "Medal Failed!! on line $LINENO"' ERR SIGTERM
+
+if [ -e $LOCKFILE ]; then
+  echo "Medal running already."
+  exit 1
+else
+  touch $LOCKFILE
+  message="Medal Started"
+  notify normal "$message" "Medal" "$message"
+  dockerImage="ghcr.io/phyzical/medal.tv-bulk-downloader"
+  docker pull $dockerImage
+  docker run \
+    -v /mnt/user/Downloads/medal/config.json:/app/config.json \
+    -v /mnt/user/Downloads/medal:/downloads \
+    $dockerImage:latest
+
+  echo "Finished Medal Download!!"
+  rm -f $LOCKFILE
+  notify normal "Finished Medal Download!!" "Finished Medal Download, it took $(elapsed_time_message $SECONDS)" ""
+  exit 0
+fi
