0e24a2c7759782f7ecfde46a6ec9c437b478cf4b
phyzical <5182053+phyzical@users.noreply.github.com>
2024-11-02 21:59:53 +0800

    remove date filter

--- scripts/unraid/youtubeScraper.sh
+++ scripts/unraid/youtubeScraper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 source $DIR/commonFuncs.sh
 
@@ -9,77 +9,70 @@ trap 'failed_func $LOCKFILE "Youtube Failed!! with code ($?)" "Youtube Failed!! 
 
 textRemovalRegex=$1
 
-if [ -e $LOCKFILE ]
-then
-    echo "Youtube running already."
-    exit 1
+if [ -e $LOCKFILE ]; then
+  echo "Youtube running already."
+  exit 1
 else
-    touch $LOCKFILE
-    message="Youtube Started"
-    notify normal $message "Youtube" $message
-    youtubePath="/mnt/user/Downloads/youtube"
-    dockerImage="phyzical/yt-dlp"
-    docker pull $dockerImage
-    for channelName in "${!urls[@]}";
-    do
-        url=${urls[$channelName]}
-        format="bv*[ext=mp4]+ba[ext=m4a]"
-        outputFormat="$channelName/processing/%(upload_date)s.%(title)s.%(ext)s"
-        oneMonthAgo="$(date -d "-1 months" '+%Y%m%d')"
-        showPath="$youtubePath/$channelName"
-        processingPath="$showPath/processing"
+  touch $LOCKFILE
+  message="Youtube Started"
+  notify normal $message "Youtube" $message
+  youtubePath="/mnt/user/Downloads/youtube"
+  dockerImage="phyzical/yt-dlp"
+  docker pull $dockerImage
+  for channelName in "${!urls[@]}"; do
+    url=${urls[$channelName]}
+    format="bv*[ext=mp4]+ba[ext=m4a]"
+    outputFormat="$channelName/processing/%(upload_date)s.%(title)s.%(ext)s"
+    oneMonthAgo="$(date -d "-1 months" '+%Y%m%d')"
+    showPath="$youtubePath/$channelName"
+    processingPath="$showPath/processing"
 
-        echo "Downloading $channelName"
-        docker run --rm -u 99:100 -v $youtubePath:/workdir:rw $dockerImage \
-        -f "$format" --download-archive "$channelName.txt" --write-thumbnail --add-metadata \
-        --no-write-playlist-metafiles --compat-options no-youtube-unavailable-videos --sponsorblock-remove "default" \
-        --write-auto-sub --cookies cookies.txt --write-info-json --convert-subs=srt --sub-lang "en" --ignore-no-formats-error \
-        --match-filter "duration>70 & !is_live & !was_live & availability!=premium_only" --no-progress \
-        --cache-dir /workdir/.cache --datebefore $oneMonthAgo --merge-output-format mp4 -o "$outputFormat" "$url"
+    echo "Downloading $channelName"
+    docker run --rm -u 99:100 -v $youtubePath:/workdir:rw $dockerImage \
+      -f "$format" --download-archive "$channelName.txt" --write-thumbnail --add-metadata \
+      --no-write-playlist-metafiles --compat-options no-youtube-unavailable-videos --sponsorblock-remove "default" \
+      --write-auto-sub --cookies cookies.txt --write-info-json --convert-subs=srt --sub-lang "en" --ignore-no-formats-error \
+      --match-filter "duration>70 & !is_live & !was_live & availability!=premium_only" --no-progress \
+      --cache-dir /workdir/.cache --merge-output-format mp4 -o "$outputFormat" "$url"
+    #  --datebefore $oneMonthAgo
+    if [ -d "$processingPath" ]; then
+      if ls $processingPath/*.webp 1>/dev/null 2>&1; then
+        echo "Converting images matching ($processingPath/*.webp)"
+        docker run --rm -v "$processingPath":/src --user=99:100 \
+          madhead/imagemagick magick mogrify -format jpg /src/*.webp
+      fi
 
-        if [ -d "$processingPath" ]; 
-        then
-            if ls $processingPath/*.webp 1> /dev/null 2>&1;
-            then
-                echo "Converting images matching ($processingPath/*.webp)"
-                docker run --rm -v "$processingPath":/src --user=99:100 \
-                madhead/imagemagick magick mogrify -format jpg /src/*.webp
-            fi
-                
-            echo "Deleting webps matching ($processingPath/*.webp)"
-            rm -rf $processingPath/*.webp
-            
-            if ls $processingPath/*.mp4 1> /dev/null 2>&1;
-            then
-                echo "generating thumnails for ($processingPath/*.mp4)"
-                thumbnail_generate "$processingPath"
-            fi
+      echo "Deleting webps matching ($processingPath/*.webp)"
+      rm -rf $processingPath/*.webp
 
-            echo "Replacing text in filename"
-            filesToRename=$(find "$processingPath" -type f | sed -e 's/ /\~_-/g')
-            for old in $filesToRename; do
-              old=$(echo "$old" | sed -e 's/\~_-/ /g')
-              new=$(echo "$old" | sed -e "s/$textRemovalRegex//g")
-              if [ "$old" != "$new" ]; then  
-                mv -v "$old" "$new"
-              fi
-            done
+      if ls $processingPath/*.mp4 1>/dev/null 2>&1; then
+        echo "generating thumnails for ($processingPath/*.mp4)"
+        thumbnail_generate "$processingPath"
+      fi
 
-            if [[ " ${manualShows[@]} " =~ " $channelName " ]];
-            then
-                echo "moving '$processingPath/*' to '$showPath/'"
-                mv $processingPath/* $showPath/ || echo "$processingPath/ must be empty"
-            else
-                move_episodes_to_season_folders "$processingPath" "$showPath"
-            fi
+      echo "Replacing text in filename"
+      filesToRename=$(find "$processingPath" -type f | sed -e 's/ /\~_-/g')
+      for old in $filesToRename; do
+        old=$(echo "$old" | sed -e 's/\~_-/ /g')
+        new=$(echo "$old" | sed -e "s/$textRemovalRegex//g")
+        if [ "$old" != "$new" ]; then
+          mv -v "$old" "$new"
         fi
-    done
+      done
 
-    remove_empty_folders "/mnt/user/Downloads/youtube/"
+      if [[ " ${manualShows[@]} " =~ " $channelName " ]]; then
+        echo "moving '$processingPath/*' to '$showPath/'"
+        mv $processingPath/* $showPath/ || echo "$processingPath/ must be empty"
+      else
+        move_episodes_to_season_folders "$processingPath" "$showPath"
+      fi
+    fi
+  done
 
-    echo "Finished Youtube Download!!"
-    rm -f $LOCKFILE
-    notify normal "Finished Youtube Download!!" "Finished Youtube Download, it took $(elapsed_time_message $SECONDS)" ""
-    exit 0
+  remove_empty_folders "/mnt/user/Downloads/youtube/"
+
+  echo "Finished Youtube Download!!"
+  rm -f $LOCKFILE
+  notify normal "Finished Youtube Download!!" "Finished Youtube Download, it took $(elapsed_time_message $SECONDS)" ""
+  exit 0
 fi
-
