1d3d0c66d596216106f34b3f640729a445670e5f
phyzical <5182053+phyzical@users.noreply.github.com>
2025-02-09 14:17:57 +0800

    simply scraper as its not the main proccessor anymore

--- scripts/unraid/youtubeScraper.sh
+++ scripts/unraid/youtubeScraper.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-source $DIR/commonFuncs.sh
+source "$DIR"/commonFuncs.sh
 
 LOCKFILE="/tmp/youtubeInProgress.lock"
 
 trap 'failed_func $LOCKFILE "Youtube Failed!! with code ($?)" "Youtube Failed!! on line $LINENO"' ERR SIGTERM
-
-textRemovalRegex=$1
 
 if [ -e $LOCKFILE ]; then
   echo "Youtube running already."
@@ -15,7 +13,7 @@ if [ -e $LOCKFILE ]; then
 else
   touch $LOCKFILE
   message="Youtube Started"
-  notify normal $message "Youtube" $message
+  notify normal "$message" "Youtube" "$message"
   youtubePath="/mnt/user/Downloads/youtube"
   dockerImage="phyzical/yt-dlp"
   docker pull $dockerImage
@@ -23,7 +21,6 @@ else
     url=${urls[$channelName]}
     format="bv*[ext=mp4]+ba[ext=m4a]"
     outputFormat="$channelName/processing/%(upload_date)s.%(title)s.%(ext)s"
-    oneMonthAgo="$(date -d "-1 months" '+%Y%m%d')"
     showPath="$youtubePath/$channelName"
     processingPath="$showPath/processing"
 
@@ -34,38 +31,22 @@ else
       --write-auto-sub --cookies cookies.txt --write-info-json --convert-subs=srt --sub-lang "en" --ignore-no-formats-error \
       --match-filter "duration>70 & !is_live & !was_live & availability!=premium_only" --no-progress \
       --cache-dir /workdir/.cache --merge-output-format mp4 -o "$outputFormat" "$url"
-    #  --datebefore $oneMonthAgo
     if [ -d "$processingPath" ]; then
-      if ls $processingPath/*.webp 1>/dev/null 2>&1; then
+      if ls "$processingPath"/*.webp 1>/dev/null 2>&1; then
         echo "Converting images matching ($processingPath/*.webp)"
         docker run --rm -v "$processingPath":/src --user=99:100 \
           madhead/imagemagick magick mogrify -format jpg /src/*.webp
       fi
 
       echo "Deleting webps matching ($processingPath/*.webp)"
-      rm -rf $processingPath/*.webp
+      rm -rf "$processingPath"/*.webp
 
-      if ls $processingPath/*.mp4 1>/dev/null 2>&1; then
+      if ls "$processingPath"/*.mp4 1>/dev/null 2>&1; then
         echo "generating thumnails for ($processingPath/*.mp4)"
         thumbnail_generate "$processingPath"
       fi
 
-      echo "Replacing text in filename"
-      filesToRename=$(find "$processingPath" -type f | sed -e 's/ /\~_-/g')
-      for old in $filesToRename; do
-        old=$(echo "$old" | sed -e 's/\~_-/ /g')
-        new=$(echo "$old" | sed -e "s/$textRemovalRegex//g")
-        if [ "$old" != "$new" ]; then
-          mv -v "$old" "$new"
-        fi
-      done
-
-      if [[ " ${manualShows[@]} " =~ " $channelName " ]]; then
-        echo "moving '$processingPath/*' to '$showPath/'"
-        mv $processingPath/* $showPath/ || echo "$processingPath/ must be empty"
-      else
-        move_episodes_to_season_folders "$processingPath" "$showPath"
-      fi
+      move_episodes_to_season_folders "$processingPath" "$showPath"
     fi
   done
 
